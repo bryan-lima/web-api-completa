ffab091745f40e692f1f232b38bb805b3baaf686
Bryan <bryanlima200@outlook.com>
2022-05-07 01:07:08 -0300

    Adicionado controllers Main e Fornecedores e iniciado desenvolvimento

---
+++ src/DevIO.Api/Controllers/FornecedoresController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using DevIO.Api.ViewModels;
+using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/fornecedores")]
+    public class FornecedoresController : MainController
+    {
+        private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IMapper _mapper;
+
+        public FornecedoresController(IFornecedorRepository fornecedorRepository, 
+                                      IMapper mapper)
+        {
+            _fornecedorRepository = fornecedorRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<FornecedorViewModel>> ObterTodos()
+        {
+            var fornecedor = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
+
+            return fornecedor;
+        }
+    }
+}
---
+++ src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+
+    }
+}
