78af9e63e628307a3b6c495d4dc22a2cb68bd592
Bryan <bryanlima200@outlook.com>
2022-05-07 00:54:11 -0300

    Configurado injeção de dependência do MeuDbContext, repositorys e entidades

---
+++ src/DevIO.Api/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,22 @@
+﻿using DevIO.Business.Intefaces;
+using DevIO.Data.Context;
+using DevIO.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<MeuDbContext>();
+            services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+
+            return services;
+        }
+    }
+}
--- src/DevIO.Api/Startup.cs
+++ src/DevIO.Api/Startup.cs
@@ -1,7 +1,10 @@
+using DevIO.Api.Configuration;
+using DevIO.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +29,10 @@ namespace DevIO.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MeuDbContext>(options => 
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             services.AddAutoMapper(typeof(Startup));
 
@@ -34,6 +41,8 @@ namespace DevIO.Api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "DevIO.Api", Version = "v1" });
             });
+
+            services.ResolveDependencies();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
