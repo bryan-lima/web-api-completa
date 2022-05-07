5ecabc4062ae5bce2e2ed50f7f5a666b6684e6bb
Bryan <bryanlima200@outlook.com>
2022-05-06 22:29:53 -0300

    Adicionado views model

---
+++ src/DevIO.Api/ViewModels/FornecedorViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.ViewModels
+{
+    public class FornecedorViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
+        public string Documento { get; set; }
+
+        public int TipoFornecedor { get; set; }
+
+        public EnderecoViewModel Endereco { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public IEnumerable<ProdutoViewModel> Produtos { get; set; }
+    }
+}
