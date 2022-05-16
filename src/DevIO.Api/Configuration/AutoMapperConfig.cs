2020ebdd81335fd379f85a548345cb26faf374d9
Bryan <bryanlima200@outlook.com>
2022-05-15 23:17:25 -0300

    Adicionado mapeamento de fornecedor ao obter dados dos produtos

--- src/DevIO.Api/Configuration/AutoMapperConfig.cs
+++ src/DevIO.Api/Configuration/AutoMapperConfig.cs
@@ -14,7 +14,10 @@ namespace DevIO.Api.Configuration
         {
             CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();
             CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
-            CreateMap<Produto, ProdutoViewModel>().ReverseMap();
+            CreateMap<Produto, ProdutoViewModel>()
+                .ForMember(dest => dest.NomeFornecedor, opt => opt.MapFrom(src => src.Fornecedor.Nome));
+
+            CreateMap<ProdutoViewModel, Produto>();
         }
     }
 }
