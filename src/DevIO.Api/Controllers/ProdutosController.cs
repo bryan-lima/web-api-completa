5b9402d8ab8dbb91c0427b258ca4e3bfb22a1363
Bryan <bryanlima200@outlook.com>
2022-05-16 00:21:27 -0300

    Movido validação de anexo para início do método UploadArquivo, antes de realizar qualquer ação

--- src/DevIO.Api/Controllers/ProdutosController.cs
+++ src/DevIO.Api/Controllers/ProdutosController.cs
@@ -87,13 +87,13 @@ namespace DevIO.Api.Controllers
 
         private bool UploadArquivo(string arquivo, string imgNome)
         {
-            var imageDataByteArray = Convert.FromBase64String(arquivo);
-
             if (string.IsNullOrEmpty(arquivo))
             {
                 NotificarErro("Forneça uma imagem para este produto!");
                 return false;
             }
+
+            var imageDataByteArray = Convert.FromBase64String(arquivo);
 
             var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/imagens", imgNome);
 
