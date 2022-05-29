2159d28fa9b02977f90f0a29f7d48604efe8fe48
Bryan <bryanlima200@outlook.com>
2022-05-29 19:17:35 -0300

    Configurado e habilitado restrição de acesso ao Swagger Somente usuários autenticados conseguem visualizar

--- src/DevIO.Api/Configuration/SwaggerConfig.cs
+++ src/DevIO.Api/Configuration/SwaggerConfig.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.DependencyInjection;
@@ -52,6 +53,8 @@ namespace DevIO.Api.Configuration
 
         public static IApplicationBuilder UseSwaggerConfig(this IApplicationBuilder app, IApiVersionDescriptionProvider provider)
         {
+            app.UseMiddleware<SwaggerAuthorizedMiddleware>();
+
             app.UseSwagger();
             app.UseSwaggerUI(options => 
             {
@@ -138,6 +141,27 @@ namespace DevIO.Api.Configuration
 
                 parameter.Required |= !routeInfo.IsOptional;
             }
+        }
+    }
+
+    public class SwaggerAuthorizedMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public SwaggerAuthorizedMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path.StartsWithSegments("/swagger") && !context.User.Identity.IsAuthenticated)
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                return;
+            }
+
+            await _next.Invoke(context);
         }
     }
 }
