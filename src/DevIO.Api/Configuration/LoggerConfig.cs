17be1f6afe5fd6331bf89a5626485c15529b1b97
Bryan <bryanlima200@outlook.com>
2022-05-30 23:16:06 -0300

    Desabilitado configuração para Elmah capturar logs inseridos manualmente

--- src/DevIO.Api/Configuration/LoggerConfig.cs
+++ src/DevIO.Api/Configuration/LoggerConfig.cs
@@ -19,16 +19,16 @@ namespace DevIO.Api.Configuration
                 o.LogId = new Guid("e2581f7f-4466-463f-82ea-924d73aa4e77");
             });
 
-            services.AddLogging(builder => 
-            {
-                builder.AddElmahIo(o =>
-                {
-                    o.ApiKey = "ab4390cb8f0a44f29ed0d05bfe79a0b2";
-                    o.LogId = new Guid("e2581f7f-4466-463f-82ea-924d73aa4e77");
-                });
+            //services.AddLogging(builder => 
+            //{
+            //    builder.AddElmahIo(o =>
+            //    {
+            //        o.ApiKey = "ab4390cb8f0a44f29ed0d05bfe79a0b2";
+            //        o.LogId = new Guid("e2581f7f-4466-463f-82ea-924d73aa4e77");
+            //    });
 
-                builder.AddFilter<ElmahIoLoggerProvider>(null, LogLevel.Warning);
-            });
+            //    builder.AddFilter<ElmahIoLoggerProvider>(null, LogLevel.Warning);
+            //});
 
             return services;
         }
