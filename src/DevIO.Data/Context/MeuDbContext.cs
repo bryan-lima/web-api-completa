42eabb860adf492f5462ca34ecccd140e18499cc
Bryan <bryanlima200@outlook.com>
2022-06-14 22:19:02 -0300

    Aplicado padrão de código em MeuDbContext

--- src/DevIO.Data/Context/MeuDbContext.cs
+++ src/DevIO.Data/Context/MeuDbContext.cs
@@ -1,15 +1,20 @@
-﻿using System;
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using DevIO.Business.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace DevIO.Data.Context
 {
     public class MeuDbContext : DbContext
     {
-        public MeuDbContext(DbContextOptions<MeuDbContext> options) : base(options) { }
+        public MeuDbContext(DbContextOptions<MeuDbContext> options) : base(options)
+        {
+
+        }
 
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Endereco> Enderecos { get; set; }
@@ -17,31 +22,31 @@ namespace DevIO.Data.Context
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            foreach (var property in modelBuilder.Model.GetEntityTypes()
-                .SelectMany(e => e.GetProperties()
-                    .Where(p => p.ClrType == typeof(string))))
+            foreach (IMutableProperty property in modelBuilder.Model.GetEntityTypes()
+                                                                    .SelectMany(entityType => entityType.GetProperties()
+                                                                    .Where(property => property.ClrType == typeof(string))))
                 property.SetColumnType("varchar(100)");
 
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
 
-            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+            foreach (IMutableForeignKey relationship in modelBuilder.Model.GetEntityTypes()
+                                                                          .SelectMany(entityType => entityType.GetForeignKeys()))
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
 
             base.OnModelCreating(modelBuilder);
         }
 
         public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
         {
-            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            foreach (EntityEntry entry in ChangeTracker.Entries()
+                                                       .Where(entry => entry.Entity.GetType()
+                                                                                   .GetProperty("DataCadastro") != null))
             {
                 if (entry.State == EntityState.Added)
-                {
                     entry.Property("DataCadastro").CurrentValue = DateTime.Now;
-                }
 
                 if (entry.State == EntityState.Modified)
-                {
                     entry.Property("DataCadastro").IsModified = false;
-                }
             }
 
             return base.SaveChangesAsync(cancellationToken);
